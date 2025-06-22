e6a12c1e93c7a2eb5e174776592d603ac5e4ebd1
unknown <nexusblade5195@gmail.com>
2025-06-22 12:16:17 +0530

    deploying 4

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -41,7 +41,6 @@ const Login = () => {
       })
       if (res.data.success) {
         localStorage.setItem("token", res.data.token);
-
         dispatch(setUser(res.data.user))
         navigate("/")
         toast.success(res.data.message)
