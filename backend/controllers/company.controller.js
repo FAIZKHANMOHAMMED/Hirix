8f06dd8d20a88c9a96d87b6ca7f749656e1d2510
unknown <nexusblade5195@gmail.com>
2025-06-22 11:21:51 +0530

    deploying 2

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -20,7 +20,6 @@ export const registerCompany = async (req, res) => {
         };
         company = await Company.create({
             name: companyName,
-            userId: req.id
         });
 
         return res.status(201).json({
